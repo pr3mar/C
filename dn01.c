3a7a342aff08423545d4899118fb2e8372cfe7ad
Mark <pr3mar@users.noreply.github.com>
2014-03-13 10:40:26 +0100

    Create dn01.c
    Sort every even number in the array

---
+++ dn01.c
@@ -0,0 +1,101 @@
+/*
+  Napišite program, ki v zaporedju podatih števil uredi soda števila v naraščajočem vrstnem redu, liha števila pa pusti pri miru.
+
+  Program naj najprej prebere dolžino zaporedja (največ 100), nato pa posamezna števila.
+  Primer: Vsebina vhoda
+  7 4 3 6 1 4 2 9
+  pomeni, da je v zaporedju 7 števil, ta števila pa so po vrsti 4, 3, 6, 1, 4, 2 in 9.
+  Program naj izpiše preurejeno zaporedje, vendar brez dolžine:
+  2 3 4 1 4 6 9
+  Torej, liha števila ostanejo na svojih mestih, soda so urejena po vrsti.
+  Števila naj bovo med seboj ločena s presledki, zadnjemu številu sledita še presledek in znak za skok v novo vrstico.
+*/
+
+#include <stdio.h>
+#define velicina 100
+
+
+void izpisi(int *pTabela, int stElementov);
+
+void urediNavadno(int *pTabela, int stElementov);
+
+void uredi(int *pTabela, int stElementov);
+
+void swap(int *x, int *y);
+
+int main()
+{
+	int stElementov;
+	int tabela[velicina];
+	int i;
+	scanf("%d", &stElementov);
+	for(i = 0; i < stElementov; i++)
+	{
+		scanf("%d", &tabela[i]);
+	}
+	
+	//izpisi(tabela, stElementov);
+	//urediNavadno(tabela, stElementov);
+	//izpisi(tabela, stElementov);
+	uredi(tabela, stElementov);
+	izpisi(tabela, stElementov);
+
+	return 0;
+}
+
+void izpisi(int *pTabela, int stElementov)
+{
+	for(int i = 0; i < stElementov; i++)
+	{
+		printf("%d ", pTabela[i]);
+	}
+	printf("\n");
+}
+
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+void urediNavadno(int *pTabela, int stElementov)
+{
+	int i, j;
+	for(i = 0; i < stElementov; i++)
+	{
+		for(j = 0; j < stElementov; j++)
+		{
+			if( pTabela[i] > pTabela[j])
+			{
+				swap(&pTabela[i], &pTabela[j]);
+			}
+		} 
+	}
+}
+
+void uredi(int *pTabela, int stElementov)
+{
+	int i, j;
+	for(i = 0; i < stElementov; i++)
+	{
+		if(pTabela[i] % 2 == 0)
+		{
+			for(j = 0; j < stElementov; j++)
+			{
+				if (pTabela[j] % 2 == 0)
+				{
+					if( pTabela[i] < pTabela[j])
+					{
+						swap(&pTabela[i], &pTabela[j]);
+					}
+				}
+			}
+		}
+		else
+		{
+			continue;
+		} 
+
+	}
+}
